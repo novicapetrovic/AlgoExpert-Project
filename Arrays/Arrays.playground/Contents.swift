2484a993fc99ba1f35e8e25f79268089d7792ab4
Novica Petrovic <novica.petrovic@clearscore.com>
2020-12-08 09:45:38 +0000

    Adding setup for Arrays

--- Arrays/Arrays.playground/Contents.swift
+++ Arrays/Arrays.playground/Contents.swift
@@ -1,3 +1,44 @@
-import UIKit
+import Foundation
 
-var str = "Hello, playground"
+// Algo Expert - Arrays
+
+// Easy - (Q1-Q2)
+// Question 1 - Two Number Sum
+
+// Question 2 - Validate SubSequence
+
+// Medium - (Q3-Q8)
+// Question 3 - Three Number Sum
+
+// Question 4 - Smallest Difference
+
+// Question 5 - Move Element To End
+
+// Question 6 - Monotonic Array
+
+// Question 7 - Spiral Traverse
+
+// Question 8 - Longest Peak
+
+// Hard - (Q9-Q13)
+// Question 9 - Four Number Sum
+
+// Question 10 - Subarray Sort
+
+// Question 11 - Largest Range
+
+// Question 12 - Min Awards
+
+// Question 13 - Zigzag Traverse
+
+// Very Hard - (Q14-Q15)
+// Question 14 - Apartment Hunting
+
+// Question 15 - Calendar Matching
+
+// New - (Q16-17)
+// Medium
+// Question 16 - Array of Products
+
+// Very Hard
+// Question 17 - Waterfall Streams
